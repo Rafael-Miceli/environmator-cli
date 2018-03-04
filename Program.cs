908496b6e59c6356bb486d8638dd44051b31a62b
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-03-04 20:57:39 -0300

    Adicionado extensão para parsear sub verbos e adicionada options para verbo config-vsts

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using CommandLine;
 
@@ -8,85 +9,31 @@ namespace environmator_cli
     {
         static int Main(string[] args)
         {
-            return CommandLine.Parser.Default.ParseArguments<ConfigVerb>(args)
-            .MapResult(
-                (ConfigVerb opts) => RunConfigAndReturnExitCode(opts),
-                errs => 1);
-            //Ler default config?           
-
-        //     if (args.Length <= 0) 
-        //     {
-        //         Console.WriteLine("Command not found try one of these: ");
-        //         ShowCommands();
-        //         return;
-        //     }
-
-        //     var commands = args.Select(c => c.ToLower()).ToArray();
-
-        //     if(commands[0] == "-h" || commands[0] == "--help")
-        //     {
-        //         ShowCommands();
-        //         return;
-        //     }
-
-        //     if(commands[0] == "config")
-        //     {
-        //         Console.WriteLine("");
-        //         Console.WriteLine("'config' still command under construction!");
-        //         Console.WriteLine("Today we only support set your VSTS code repo environment");
-
-        //         if(commands.Length == 1) 
-        //         {
-        //             ShowConfigSubCommandsAndOptions();
-        //             return;
-        //         }
-
-        //         if(commands[1] == "vsts")
-        //         {
-        //             if(commands.Length == 2)
-        //             {
-        //                 ShowVstsOptions();
-        //                 return;
-        //             }
-
-
-        //         }
-
-        //         return;
+            return Parser.Default.ParseVerbs<ConfigVerb, NewVerb>(args)
+                 .MapResult(
+                 (ConfigVerb opts) => RunConfigAndReturnExitCode(opts),
+                 (NewVerb opts) => RunNewAndReturnExitCode(opts),
+                 (ConfigVerb.ConfigVstsVerb opts) => RunConfigVstsAndReturnExitCode(opts),
+                 errs => 1);            
         }
 
-        private static int RunConfigAndReturnExitCode(ConfigVerb opts)
+        private static int RunConfigAndReturnExitCode(object opts)
         {
             Console.WriteLine("config foi chamado");
             return 0;
         }
 
+        private static int RunNewAndReturnExitCode(object opts)
+        {
+            Console.WriteLine("new foi chamado");
+            return 0;
+        }
 
-        //     Console.WriteLine("Command not found try one of these: ");
-        //     ShowCommands();
-        // }
-
-        // private static void ShowVstsOptions()
-        // {
-        //     Console.WriteLine("");
-        //     Console.WriteLine("Options:");
-        //     Console.WriteLine("   -i --instance      your vsts instance.");
-        // }
-
-        // private static void ShowCommands()
-        // {
-        //     Console.WriteLine("");
-        //     Console.WriteLine("Commands:");
-        //     Console.WriteLine("   new       Create a new project in all environments defined in default configuration or by the parameters.");
-        //     Console.WriteLine("   config    Show the configuration to access you repositories, platforms, and C.I. builders.");
-        // }
-
-        // private static void ShowConfigSubCommandsAndOptions()
-        // {
-        //     Console.WriteLine("");
-        //     Console.WriteLine("SubCommands:");
-        //     Console.WriteLine("   vsts      Set the options to use your VSTS environment.");
-        // }
+        private static int RunConfigVstsAndReturnExitCode(object opts)
+        {
+            Console.WriteLine("config vsts foi chamado");
+            return 0;
+        }        
 
     }
 }
--- Verbs.cs
+++ Verbs.cs
@@ -3,13 +3,31 @@ using CommandLine;
 
 namespace environmator_cli
 {
-    [Verb("config", HelpText = "Show the configuration to access you repositories, platforms, and C.I. builders.")]
+    [Verb("config", HelpText = "Show the configuration to access you repositories, platforms, and C.I. builders. 'config' command still under construction! Today we only support set your VSTS code repo environment.")]
+    [ChildVerbs(typeof(ConfigVstsVerb))]
     public class ConfigVerb
     {
         public ConfigVerb()
         {
-            //Console.WriteLine("Show the configuration to access you repositories, platforms, and C.I. builders.");
+            Console.WriteLine("config Verb called");
         }
 
+        [Verb("vsts", HelpText = "Set the options to use your VSTS environment.")]
+        public class ConfigVstsVerb
+        {
+            [Option('i', "instance", HelpText = "Your vsts instance.")]
+            public string Instance { get; set; }
+        }
     }
+
+    [Verb("new", HelpText = "Create a new project in all environments defined in default configuration or by the parameters.")]
+    public class NewVerb
+    {
+        public NewVerb()
+        {
+            Console.WriteLine("new Verb called");
+        }
+    }
+
+    
 }
