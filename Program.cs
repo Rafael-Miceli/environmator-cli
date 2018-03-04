1cea7eae7b186495cde7c1923f99e63e16c22572
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-03-03 22:49:08 -0300

    Testando verbo com CommanLineParser

--- Program.cs
+++ Program.cs
@@ -1,80 +1,91 @@
 ﻿using System;
 using System.Linq;
+using CommandLine;
 
 namespace environmator_cli
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            return CommandLine.Parser.Default.ParseArguments<ConfigVerb>(args)
+            .MapResult(
+            (ConfigVerb opts) => RunAddAndReturnExitCode(opts),
+            errs => 1);
             //Ler default config?           
 
-            if (args.Length <= 0) 
-            {
-                Console.WriteLine("Command not found try one of these: ");
-                ShowCommands();
-                return;
-            }
+        //     if (args.Length <= 0) 
+        //     {
+        //         Console.WriteLine("Command not found try one of these: ");
+        //         ShowCommands();
+        //         return;
+        //     }
 
-            var commands = args.Select(c => c.ToLower()).ToArray();
+        //     var commands = args.Select(c => c.ToLower()).ToArray();
 
-            if(commands[0] == "-h" || commands[0] == "--help")
-            {
-                ShowCommands();
-                return;
-            }
+        //     if(commands[0] == "-h" || commands[0] == "--help")
+        //     {
+        //         ShowCommands();
+        //         return;
+        //     }
 
-            if(commands[0] == "config")
-            {
-                Console.WriteLine("");
-                Console.WriteLine("'config' still command under construction!");
-                Console.WriteLine("Today we only support set your VSTS code repo environment");
+        //     if(commands[0] == "config")
+        //     {
+        //         Console.WriteLine("");
+        //         Console.WriteLine("'config' still command under construction!");
+        //         Console.WriteLine("Today we only support set your VSTS code repo environment");
 
-                if(commands.Length == 1) 
-                {
-                    ShowConfigSubCommandsAndOptions();
-                    return;
-                }
+        //         if(commands.Length == 1) 
+        //         {
+        //             ShowConfigSubCommandsAndOptions();
+        //             return;
+        //         }
 
-                if(commands[1] == "vsts")
-                {
-                    if(commands.Length == 2)
-                    {
-                        ShowVstsOptions();
-                        return;
-                    }
-                    
-                                            
-                }
+        //         if(commands[1] == "vsts")
+        //         {
+        //             if(commands.Length == 2)
+        //             {
+        //                 ShowVstsOptions();
+        //                 return;
+        //             }
 
-                return;
-            }
 
-            
-            Console.WriteLine("Command not found try one of these: ");
-            ShowCommands();
+        //         }
+
+        //         return;
         }
 
-        private static void ShowVstsOptions()
+        private static int RunAddAndReturnExitCode(ConfigVerb opts)
         {
-            Console.WriteLine("");
-            Console.WriteLine("Options:");
-            Console.WriteLine("   -i --instance      your vsts instance.");
+            throw new NotImplementedException();
         }
 
-        private static void ShowCommands()
-        {
-            Console.WriteLine("");
-            Console.WriteLine("Commands:");
-            Console.WriteLine("   new       Create a new project in all environments defined in default configuration or by the parameters.");
-            Console.WriteLine("   config    Show the configuration to access you repositories, platforms, and C.I. builders.");
-        }
 
-        private static void ShowConfigSubCommandsAndOptions()
-        {
-            Console.WriteLine("");
-            Console.WriteLine("SubCommands:");
-            Console.WriteLine("   vsts      Set the options to use your VSTS environment.");
-        }
+        //     Console.WriteLine("Command not found try one of these: ");
+        //     ShowCommands();
+        // }
+
+        // private static void ShowVstsOptions()
+        // {
+        //     Console.WriteLine("");
+        //     Console.WriteLine("Options:");
+        //     Console.WriteLine("   -i --instance      your vsts instance.");
+        // }
+
+        // private static void ShowCommands()
+        // {
+        //     Console.WriteLine("");
+        //     Console.WriteLine("Commands:");
+        //     Console.WriteLine("   new       Create a new project in all environments defined in default configuration or by the parameters.");
+        //     Console.WriteLine("   config    Show the configuration to access you repositories, platforms, and C.I. builders.");
+        // }
+
+        // private static void ShowConfigSubCommandsAndOptions()
+        // {
+        //     Console.WriteLine("");
+        //     Console.WriteLine("SubCommands:");
+        //     Console.WriteLine("   vsts      Set the options to use your VSTS environment.");
+        // }
+
     }
 }
---
+++ Verbs.cs
@@ -0,0 +1,10 @@
+using CommandLine;
+
+namespace environmator_cli
+{
+    [Verb("config", HelpText = "Show the configuration to access you repositories, platforms, and C.I. builders.")]
+    public class ConfigVerb
+    {
+
+    }
+}
