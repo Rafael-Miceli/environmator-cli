ee1dfa59a840f24e2e7581ed93f6d68e3873eae5
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-03-10 10:12:08 -0300

    Adicionado repositorio para gerenciar configurações

---
+++ Configuration/IConfigRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace environmator_cli.Configuration
+{
+    public class ConfigRepository
+    {
+        internal void SetVstsConfig(ConfigVerb.ConfigVstsVerb opts)
+        {
+            Console.WriteLine($"Setando valor do vsts {opts.Instance} - {opts.Project}");
+        }
+    }
+}
