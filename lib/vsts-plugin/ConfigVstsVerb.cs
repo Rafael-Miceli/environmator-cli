8c19338a21ca62679516d953d3041e3bdd86a0df
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-03-30 10:53:37 -0300

    Adicionando leitor de configs para plugins

---
+++ lib/vsts-plugin/ConfigVstsVerb.cs
@@ -0,0 +1,16 @@
+﻿using ci_x_core;
+using CommandLine;
+
+namespace vsts_plugin
+{
+    [Verb("vsts", HelpText = "Set the options to use your VSTS environment.")]
+    public class ConfigVstsVerb: ConfigVerb
+    {
+        [Option('i', "instance", HelpText = "Your vsts instance.", Required = true)]
+        public string Instance { get; set; }
+        [Option('p', "project", HelpText = "Your vsts project.", Required = true)]
+        public string Project { get; set; }
+        [Option('t', "token", HelpText = "Your vsts personal token, to see how to generate a personal token follow this link: https://docs.microsoft.com/en-us/vsts/accounts/use-personal-access-tokens-to-authenticate?view=vsts", Required = true)]
+        public string Token { get; set; }
+    }
+}
---
+++ src/ci-x-core/ConfigVerb.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ci_x_core
+{
+    public class ConfigVerb
+    {
+    }
+}
