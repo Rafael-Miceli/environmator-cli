471e211f678a9b3385bdaa0bb1bc1368d6bbc3e4
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-03-31 16:23:45 -0300

    Implementando ConfigRepository

--- lib/vsts-plugin/VstsConfig.cs
+++ lib/vsts-plugin/VstsConfig.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using ci_x_core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace vsts_plugin
 {
-    public class VstsConfig
+    public class VstsConfig : ConfigRepository<ConfigVstsVerb>
     {
+        public VstsConfig(): base("vsts")
+        {
+
+        }
+
+        public override ConfigVstsVerb ReadDefinedConfigSections(IEnumerable<string[]> pluginConfigSplited)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string[] DefinePluginSection()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
